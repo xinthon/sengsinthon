9369295884c5f5867b3a3ece050917de8fe3bf72
Sinthon SENG <110073771+xinthon@users.noreply.github.com>
2024-09-27 14:32:40 +0700

    improve => hide swoosh  for (lg screen)

--- src/components/containers/withSwoosh/index.tsx
+++ src/components/containers/withSwoosh/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function WithSwoosh() {
   return (
-    <div className="pointer-events-none fixed bottom-0 right-0 z-100 origin-bottom-right scale-75 bg-transparent bg-[url('https://storage.googleapis.com/candycode/bg.png')] bg-[length:100%_100%] bg-no-repeat pl-[256px] pt-[96px] leading-none sm:scale-100">
+    <div className="pointer-events-none fixed bottom-0 right-0 z-100 origin-bottom-right scale-75 bg-transparent bg-[url('https://storage.googleapis.com/candycode/bg.png')] bg-[length:100%_100%] bg-no-repeat pl-[256px] pt-[96px] leading-none sm:scale-100 hidden lg:block">
       <a
         href="https://www.linkedin.com/in/seng-sinthon-285905256"
         target="_blank"
